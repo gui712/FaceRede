f84d2c1a80883999d6daccc3be659354841e10c1
Guilherme Costa <guilherme.csilva@userede.com.br>
2021-08-27 18:45:44 -0300

    Iniciar projeto

---
+++ src/main/java/br/com/userede/RedeFace/RedeFaceApplication.java
@@ -0,0 +1,13 @@
+package br.com.userede.RedeFace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedeFaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedeFaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/userede/RedeFace/RedeFaceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.userede.RedeFace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeFaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
