fd5b9972e5c37fcdd8e536fa2b513526690e1267
Guilherme Costa <guilherme.csilva@userede.com.br>
2021-08-28 20:03:31 -0300

    Criação interface UsuarioRepository

---
+++ src/main/java/br/com/userede/RedeFace/user/UsuarioRepository.java
@@ -0,0 +1,6 @@
+package br.com.userede.RedeFace.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, String> {
+}
