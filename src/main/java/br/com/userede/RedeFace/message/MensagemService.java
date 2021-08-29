e057c493d2dac0f35602a4eb87f7c9c10318b594
Guilherme Costa <guilherme.csilva@userede.com.br>
2021-08-28 23:00:55 -0300

    Atualizando metodos a classe MensagemService

--- src/main/java/br/com/userede/RedeFace/message/MensagemService.java
+++ src/main/java/br/com/userede/RedeFace/message/MensagemService.java
@@ -18,7 +18,14 @@ public class MensagemService {
         this.mensagemRepository = mensagemRepository;
         this.usuarioService = usuarioService;
     }
-
+    public Mensagem visualizarMensagem(int id){
+        if(mensagemExistente(id)){
+            Mensagem pesquisaMensagem = buscarMensagemId(id);
+            pesquisaMensagem.setVisualizado(true);
+            return pesquisaMensagem;
+        }
+        throw new RuntimeException("Mensagem não foi encontrada!");
+    }
     public void EnviarMensagem(String emailOrigem, String emailDestino, String mensagem) throws NotFoundException {
         Mensagem novaMensagem = new Mensagem();
         if (usuarioService.usuarioExistente(emailOrigem) & usuarioService.usuarioExistente(emailDestino)){
@@ -34,15 +41,17 @@ public class MensagemService {
             throw new RuntimeException("Email não encontrado! ");
         }
     }
-    public Boolean mensagemExistente(int id){
-        return mensagemRepository.existsById(id);
-    }
+
     public Mensagem buscarMensagemId(int id){
         Optional<Mensagem> optionalMensagem = mensagemRepository.findById(id);
         if (optionalMensagem.isPresent()){
             return optionalMensagem.get();
         }
         throw new RuntimeException("Mensagem não foi encontrada!");
+    }
+
+    public Boolean mensagemExistente(int id){
+        return mensagemRepository.existsById(id);
     }
 
 
