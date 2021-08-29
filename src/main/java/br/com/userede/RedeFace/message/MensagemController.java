d1561410581b2db745691c04734b95904d43c8ac
Guilherme Costa <guilherme.csilva@userede.com.br>
2021-08-28 23:17:00 -0300

    Configurando Classe Mensagem Controller

--- src/main/java/br/com/userede/RedeFace/message/MensagemController.java
+++ src/main/java/br/com/userede/RedeFace/message/MensagemController.java
@@ -1,4 +1,25 @@
 package br.com.userede.RedeFace.message;
 
-public class MensagemController ]{
-        }
+import br.com.userede.RedeFace.message.dtos.CadastroMensagemDto;
+import javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/chat")
+public class MensagemController {
+    @Autowired
+    private MensagemService mensagemService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void enviarMensagem(@RequestBody CadastroMensagemDto mensagemDto) throws NotFoundException {
+        mensagemService.EnviarMensagem(mensagemDto.getOrigem(), mensagemDto.getDestino(), mensagemDto.getMensagem());
+    }
+
+    @GetMapping("/{idMensagem}")
+    public Mensagem visualizarMensagem(@PathVariable int idMensagem){
+        return mensagemService.visualizarMensagem(idMensagem);
+    }
+
+}
