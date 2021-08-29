e64df53442b2eb55d2822365c29e417f8381d254
Guilherme Costa <guilherme.csilva@userede.com.br>
2021-08-29 12:43:20 -0300

    Configuração classe Erro

---
+++ src/main/java/br/com/userede/RedeFace/exceptions/Erro.java
@@ -0,0 +1,27 @@
+package br.com.userede.RedeFace.exceptions;
+
+public class Erro {
+    private String campo;
+    private String mensagem;
+
+    public Erro(String campo, String mensagem) {
+        this.campo = campo;
+        this.mensagem = mensagem;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public void setCampo(String campo) {
+        this.campo = campo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
---
+++ src/main/java/br/com/userede/RedeFace/exceptions/MensagemErro.java
@@ -0,0 +1,8 @@
+package br.com.userede.RedeFace.exceptions;
+
+import java.util.List;
+
+public class MensagemErro {
+    private int statusCode;
+    private List<Error>
+}
