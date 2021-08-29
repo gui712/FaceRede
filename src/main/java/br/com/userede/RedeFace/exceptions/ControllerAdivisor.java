6a909883c153004a826ecbe49efc1f329a3c3fde
Guilherme Costa <guilherme.csilva@userede.com.br>
2021-08-29 12:50:55 -0300

    Configuração Classe ControllerAdivisor

--- src/main/java/br/com/userede/RedeFace/exceptions/ControllerAdivisor.java
+++ src/main/java/br/com/userede/RedeFace/exceptions/ControllerAdivisor.java
@@ -1,13 +1,22 @@
 package br.com.userede.RedeFace.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class ControllerAdivisor {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MensagemErro controlarExecaoValidacao(MethodArgumentNotValidException exception){
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        List<Erro> erros = fieldErrors.stream().map(obj -> new Erro(obj.getField(), obj.getDefaultMessage())).collect(Collectors.toList());
+        return new MensagemErro(400,erros);
+    }
 }
--- src/main/java/br/com/userede/RedeFace/exceptions/MensagemErro.java
+++ src/main/java/br/com/userede/RedeFace/exceptions/MensagemErro.java
@@ -1,8 +1,30 @@
 package br.com.userede.RedeFace.exceptions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MensagemErro {
     private int statusCode;
-    private List<Error>
+    private List<Erro> erros = new ArrayList<>();
+
+    public MensagemErro(int statusCode, List<Erro> erros) {
+        this.statusCode = statusCode;
+        this.erros = erros;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public List<Erro> getErros() {
+        return erros;
+    }
+
+    public void setErros(List<Erro> erros) {
+        this.erros = erros;
+    }
 }
