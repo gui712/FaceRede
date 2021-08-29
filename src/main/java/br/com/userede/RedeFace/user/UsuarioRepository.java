65be6db81a28a86713622aa75190fae84636287d
Guilherme Costa <guilherme.csilva@userede.com.br>
2021-08-28 21:25:10 -0300

    Criação lista de mensagens não visualizadas dentro da classe UsuarioRepository

--- src/main/java/br/com/userede/RedeFace/user/UsuarioRepository.java
+++ src/main/java/br/com/userede/RedeFace/user/UsuarioRepository.java
@@ -1,6 +1,12 @@
 package br.com.userede.RedeFace.user;
 
+import br.com.userede.RedeFace.message.Mensagem;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UsuarioRepository extends CrudRepository<Usuario, String> {
+    @Query("select m from Mensagem m where m.visualizado = false and m.destino.email =?!")
+    public List<Mensagem> mostrarMensagemNãoVisualizada(String email);
 }
