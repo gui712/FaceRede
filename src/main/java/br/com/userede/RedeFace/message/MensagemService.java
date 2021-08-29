dbab6a87849cbe3b6c8734d101e9e2a6d3397ff3
Guilherme Costa <guilherme.csilva@userede.com.br>
2021-08-28 21:53:23 -0300

    Adicionando metodos na classe MensagemService

--- src/main/java/br/com/userede/RedeFace/message/MensagemService.java
+++ src/main/java/br/com/userede/RedeFace/message/MensagemService.java
@@ -1,7 +1,12 @@
 package br.com.userede.RedeFace.message;
 
+import br.com.userede.RedeFace.user.Usuario;
+import br.com.userede.RedeFace.user.UsuarioService;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class MensagemService {
@@ -14,7 +19,31 @@ public class MensagemService {
         this.usuarioService = usuarioService;
     }
 
-    public void EnviarMensagem(String emailOrigem, String emailDestino, String mensagem){
+    public void EnviarMensagem(String emailOrigem, String emailDestino, String mensagem) throws NotFoundException {
         Mensagem novaMensagem = new Mensagem();
+        if (usuarioService.usuarioExistente(emailOrigem) & usuarioService.usuarioExistente(emailDestino)){
+            Usuario usuarioOrigem = usuarioService.buscarUsuarioPorEmail(emailOrigem);
+            novaMensagem.setOrigem(usuarioOrigem);
+
+            Usuario usuarioDestino = usuarioService.buscarUsuarioPorEmail(emailDestino);
+            novaMensagem.setMessage(mensagem);
+            novaMensagem.setVisualizado(false);
+            novaMensagem.setDestino(usuarioDestino);
+            mensagemRepository.save(novaMensagem);
+        }else{
+            throw new RuntimeException("Email não encontrado! ");
+        }
     }
+    public Boolean mensagemExistente(int id){
+        return mensagemRepository.existsById(id);
+    }
+    public Mensagem buscarMensagemId(int id){
+        Optional<Mensagem> optionalMensagem = mensagemRepository.findById(id);
+        if (optionalMensagem.isPresent()){
+            return optionalMensagem.get();
+        }
+        throw new RuntimeException("Mensagem não foi encontrada!");
+    }
+
+
 }
