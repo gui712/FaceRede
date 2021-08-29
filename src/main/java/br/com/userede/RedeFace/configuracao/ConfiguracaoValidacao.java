7aecbca467f289053817cc25200e879d1afd67e8
Guilherme Costa <guilherme.csilva@userede.com.br>
2021-08-29 12:27:33 -0300

    Configuração pacote configuraçãoValidacao

---
+++ src/main/java/br/com/userede/RedeFace/configuracao/ConfiguracaoValidacao.java
@@ -0,0 +1,25 @@
+package br.com.userede.RedeFace.configuracao;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Component
+public class ConfiguracaoValidacao {
+    @Bean
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:MensagensDeValidacao");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator(){
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+}
